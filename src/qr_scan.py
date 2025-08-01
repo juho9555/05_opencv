35a0ef08f1097faf6b880a002af1ebc8973b623d
JUHO <ijuho9555@gmail.com>
2025-08-01 11:42:52 +0900

    웹캠으로 qr인식 하기

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -1,27 +1,41 @@
-import cv2
-import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
+import cv2
 
-img = cv2.imread('../img/frame.png')
+cap = cv2.VideoCapture(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+i = 0
+while(cap.isOpened()):
+  ret, img = cap.read()
 
+  if not ret:
+    continue
 
-#plt.imshow(img)
-#plt.imshow(gray, cmap='gray')
-#plt.show()
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+     
+  decoded = pyzbar.decode(gray)
+  #print(f"감지된 개수 :, {len(decoded)}")
 
-# 디코딩
-decoded = pyzbar.decode(gray)
-print(decoded)
+  for d in decoded: 
+    x, y, w, h = d.rect
 
-for d in decoded:
-    print(d.data.decode('utf-8'))
-    print(d.type)
+    barcode_data = d.data.decode("utf-8")
+    barcode_type = d.type
 
-    cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0, 255, 0), 15)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-plt.imshow(img)
-plt.show()
-cv2.waitKey(0)
+    text = '%s (%s)' % (barcode_data, barcode_type)
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+
+  cv2.imshow('img', img)
+  # for 루프 밖에 추가 (테스트용)
+  #cv2.rectangle(img, (100, 100), (200, 200), (0, 255, 0), 2)  # 초록색 테스트 사각형``
+
+  key = cv2.waitKey(1)
+  if key == ord('q'):
+    break
+  elif key == ord('s'):
+    i += 1
+    cv2.imwrite('c_%03d.jpg' % i, img)
+
+cap.release()
 cv2.destroyAllWindows()
