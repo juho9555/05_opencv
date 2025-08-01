b981f3a64c7dafa1d453f634d9eb06216aa30e08
JUHO <ijuho9555@gmail.com>
2025-08-01 12:05:12 +0900

    웹캠으로 인식한 qr코드 사이트 접속

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -1,9 +1,12 @@
 import pyzbar.pyzbar as pyzbar
 import cv2
+import webbrowser
 
 cap = cv2.VideoCapture(0)
 
 i = 0
+visited_urls = set() # url 중복으로 들어가기 방지
+
 while(cap.isOpened()):
   ret, img = cap.read()
 
@@ -11,24 +14,23 @@ while(cap.isOpened()):
     continue
 
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-     
   decoded = pyzbar.decode(gray)
-  #print(f"감지된 개수 :, {len(decoded)}")
 
   for d in decoded: 
     x, y, w, h = d.rect
-
     barcode_data = d.data.decode("utf-8")
     barcode_type = d.type
 
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
     text = '%s (%s)' % (barcode_data, barcode_type)
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
 
+    # url접속 코드
+    if barcode_data.startswith("http") and barcode_data not in visited_urls:
+      webbrowser.open(barcode_data)
+      visited_urls.add(barcode_data)
+
   cv2.imshow('img', img)
-  # for 루프 밖에 추가 (테스트용)
-  #cv2.rectangle(img, (100, 100), (200, 200), (0, 255, 0), 2)  # 초록색 테스트 사각형``
 
   key = cv2.waitKey(1)
   if key == ord('q'):
